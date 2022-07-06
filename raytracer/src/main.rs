f27cd5f8543e9a6e8d548299286d5d91bd662c82
czj0xyz <czj0xyz@gmail.com>
2022-07-06 14:33:20 +0800

    picture2_2_Done

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,62 +1,23 @@
-use std::{fs::File, process::exit};
+const HEIGHT : usize = 256;
+const WIDTH : usize = 256;
+fn main(){
 
-use image::{ImageBuffer, RgbImage};
-
-use console::style;
-use indicatif::{ProgressBar, ProgressStyle};
-
-fn main() {
-    print!("{}[2J", 27 as char); // Clear screen
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Set cursor position as 1,1
-
-    let height = 800;
-    let width = 800;
-    let quality = 60; // From 0 to 100
-    let path = "output/output.jpg";
-
-    println!(
-        "Image size: {}\nJPEG quality: {}",
-        style(width.to_string() + &"x".to_string() + &height.to_string()).yellow(),
-        style(quality.to_string()).yellow(),
-    );
-
-    // Create image data
-    let mut img: RgbImage = ImageBuffer::new(width, height);
-    // Progress bar UI powered by library `indicatif`
-    // Get environment variable CI, which is true for GitHub Action
-    let progress = if option_env!("CI").unwrap_or_default() == "true" {
-        ProgressBar::hidden()
-    } else {
-        ProgressBar::new((height * width) as u64)
-    };
-    progress.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] [{pos}/{len}] ({eta})")
-        .progress_chars("#>-"));
-
-    // Generate image
-    for y in 0..height {
-        for x in 0..width {
-            let pixel_color = [
-                (y as f32 / height as f32 * 255.).floor() as u8,
-                ((x + height - y) as f32 / (height + width) as f32 * 255.).floor() as u8,
-                (x as f32 / height as f32 * 255.).floor() as u8,
-            ];
-            let pixel = img.get_pixel_mut(x, height - y - 1);
-            *pixel = image::Rgb(pixel_color);
-            progress.inc(1);
+    println!("P3");    
+    println!("{} {}",WIDTH,HEIGHT);
+    println!("255");
+    for j in (0..HEIGHT).rev() {
+        eprintln!("Scanlines remaining:{}",j);
+        for i in 0..WIDTH {
+            let r = (i as f64) / ((HEIGHT-1) as f64);
+            let g = (j as f64) / ((WIDTH-1) as f64);
+            let b = 0.25;
+            
+            let ir : usize =  (r*255.999) as usize; 
+            let ig : usize =  (g*255.999) as usize;
+            let ib : usize =  (b*255.999) as usize;
+            
+            println!("{} {} {}",ir,ig,ib);
         }
     }
-    progress.finish();
-
-    // Output image to file
-    println!("Ouput image as \"{}\"", style(path).yellow());
-    let output_image = image::DynamicImage::ImageRgb8(img);
-    let mut output_file = File::create(path).unwrap();
-    match output_image.write_to(&mut output_file, image::ImageOutputFormat::Jpeg(quality)) {
-        Ok(_) => {}
-        // Err(_) => panic!("Outputting image fails."),
-        Err(_) => println!("{}", style("Outputting image fails.").red()),
-    }
-
-    exit(0);
+    eprintln!("Done!");
 }
