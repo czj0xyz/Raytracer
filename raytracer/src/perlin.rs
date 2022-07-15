84f0ecfc56a4797c283c1f749fa3d1eece9fb6b3
czj0xyz <czj0xyz@gmail.com>
2022-07-15 09:32:39 +0800

    Book2 Image8

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -29,15 +29,42 @@ impl Perlin {
         Perlin::permute(&mut ret);
         ret
     }
-    pub fn noise(&self, p: Point3) -> f64 {
-        // if p.x()<0.0 || p.y()<0.0 || p.z()<0.0 {
-        //     eprintln!("????");
-        // }
-        let i = (((4.0 * p.x()) as isize) & 255) as usize;
-        let j = (((4.0 * p.y()) as isize) & 255) as usize;
-        let k = (((4.0 * p.z()) as isize) & 255) as usize;
 
-        (*self).ranfloat[(*self).perm_x[i] ^ (*self).perm_y[j] ^ (*self).perm_z[k]]
+    fn trilinear_interp(c: Vec<f64>, u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += ((i as f64) * u + ((1 - i) as f64) * (1.0 - u))
+                        * ((j as f64) * v + ((1 - j) as f64) * (1.0 - v))
+                        * ((k as f64) * w + ((1 - k) as f64) * (1.0 - w))
+                        * c[i << 2 | j << 1 | k];
+                }
+            }
+        }
+        accum
+    }
+
+    pub fn noise(&self, p: Point3) -> f64 {
+        let u_fl = p.x() - p.x().floor();
+        let v_fl = p.y() - p.y().floor();
+        let w_fl = p.z() - p.z().floor();
+        let i = p.x().floor() as isize;
+        let j = p.y().floor() as isize;
+        let k = p.z().floor() as isize;
+        let mut ret: Vec<f64> = Default::default();
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    ret.push(
+                        (*self).ranfloat[(*self).perm_x[((i + di) & 255) as usize]
+                            ^ (*self).perm_y[((j + dj) & 255) as usize]
+                            ^ (*self).perm_z[((k + dk) & 255) as usize]],
+                    );
+                }
+            }
+        }
+        Perlin::trilinear_interp(ret, u_fl, v_fl, w_fl)
     }
 }
 
